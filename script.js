b318097d88f8ff8ef4eb7ce2f279ba6f8e25b2d2
Matteo Calderaro <matteo.calderaro@binetwork.it>
2025-06-27 21:10:08 +0200

    inserita modale

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('script')
+function validateTable1() {
+            // Optionally, validate and populate error list here
+            const errorModalEl = document.getElementById('errorModal');
+            const errorModal = new bootstrap.Modal(errorModalEl);
+            errorModal.show();
+        }
